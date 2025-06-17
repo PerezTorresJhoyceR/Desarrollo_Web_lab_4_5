18883723f76cac9b714ed46c085d4bdc4a8740dd
PerezTorresJhoyceR <102771904+PerezTorresJhoyceR@users.noreply.github.com>
2025-06-17 16:10:47 -0400

    Delete LAb_5 directory

--- LAb_5/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$con = mysqli_connect("localhost", "root", "", "bd_hotel");
-if(mysqli_connect_errno()) {
-    die("Error al conectar con la base de datos: " . mysqli_connect_error());
-} else {
-    // Descomenta la siguiente línea para verificar la conexión (recuerda comentarla después de verificar)
-    //echo "¡Conexión exitosa a la base de datos bd_hotel!";
-}
-?>
